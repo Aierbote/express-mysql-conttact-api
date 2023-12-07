ae78cd0ac752a6c3b396f716eae2681ab14bafa4
Alberto Cangialosi <billyballo.res@gmail.com>
2023-12-07 10:38:08 +0100

    fix: correct flow of events
    connect -> create db -> create table
           |            `-> table exists
           `-> db exits

--- create-one-object.js
+++ create-one-object.js
@@ -3,9 +3,11 @@ const mysql = require("mysql");
 
 // variables
 const dbName = "NodeDB";
+const tableName = "Contatti";
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "root"
+  user: "root",
+  database: dbName
 });
 
 connection.connect((err) => {
@@ -21,19 +23,25 @@ connection.connect((err) => {
     // IN CASE: DB ALREADY EXISTING
     if (err) {
       console.log(`db ${dbName} already exists.`);
+    }
+    else {
+      console.log(`Database ${dbName} succesfully created! 🎉`)
+    };
+  });
 
-      // IN CASE: CREATE TABLE
-      const sql = `CREATE TABLE ${tableName} (nome VARCHAR(255), telefono VARCHAR(13), email VARCHAR(255)) `;
-      connection.query(sql, (err, result) => {
-        if (err) {
-          console.log(`It seems that table ${tableName} already exists.`);
-          throw err;
-        };
 
-        console.log(`TABLE ${tableName} succesfully created! 🎉`)
-      });
+  // IN CASE: CREATE TABLE
+  const sql = `CREATE TABLE ${tableName} (nome VARCHAR(255), telefono VARCHAR(13), email VARCHAR(255)) `;
+  connection.query(sql, (err, result) => {
+    if (err) {
+      console.log(`It seems that table ${tableName} already exists.`);
+
+      // throw err;
+      console.error(err.message);
+    }
+    else {
+      console.log(`TABLE ${tableName} succesfully created! 🎉`)
     };
 
-    console.log(`Database ${dbName} succesfully created! 🎉`)
   });
 })
