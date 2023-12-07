fe1e7e91d7a9db027341689f9eb69c8bfae66a17
Alberto Cangialosi <billyballo.res@gmail.com>
2023-12-07 23:03:12 +0100

    feat: GET '/' complete

--- routes/contatti.js
+++ routes/contatti.js
@@ -1,14 +1,43 @@
 // imports
 const express = require("express");
+const mysql = require("mysql");
 
 // variables
 let msg; // message to render in html as response
 let sql; // query for each METHOD
 const router = express.Router();
+const conn = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  database: "NodeDB",
+});
+const tableName = "Contatti";
+
+router.use(express.json());
 
 router.get("/", (req, res) => {
   msg = `<h1>📇 Contatti route is displaying data</h1>`;
-  res.send(msg);
+  sql = `SELECT * FROM ${tableName}`;
+
+  conn.query(sql, (err, results) => {
+    if (err) {
+      console.error(`Problem during GET '/': ${err.message}`);
+      res.status(500).send('Errore del server');
+    }
+
+    // // raw result
+    // res.json(results);
+
+    msg = `
+      ${msg}
+      <p>QUERY: ${sql}</p>
+      <p>Results:</p>
+      <!-- TODO: make it into a list with forEach() -->
+      <p>${JSON.stringify(results)}</p>
+    `;
+    res.send(msg);
+  });
 })
 
 router.get("/:id", (req, res) => {
