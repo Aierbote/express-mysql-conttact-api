cb63e7659863d997900694e3107b4e2854013e6b
Alberto Cangialosi <billyballo.res@gmail.com>
2023-12-21 22:15:26 +0100

    feat: working create collection to mongo atlas
    - using environment variable for secure password
    - pinging the server with `await client.db(admin).command({ ping: 1 })`
    - in order to see any effect it executes an `insertOne()`

---
+++ create-collection-mongo-atlas.js
@@ -0,0 +1,54 @@
+// // imports
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require("dotenv");
+
+dotenv.config();  // Loads variables from `.env` file into `process.env`
+// NOTE : Remember to include the `.env` file in your project's version `.gitignore`
+const psswrd = process.env.MY_MONGO_ATLAS_PSSWRD;
+
+
+// variables
+const uri = `mongodb+srv://albertocangialosi-node:${psswrd}@eserciziacasa.w9yphbr.mongodb.net/?retryWrites=true&w=majority"`;
+const dbName = 'NodeDB'; // Database Name
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    console.log("CIAO: ci stiamo connettendo! ");
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    console.log("CIAO: ci siamo connessi! ");
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const db = client.db(dbName);
+    const collection = db.collection('contatti');
+
+    const insertResult = await collection.insertOne({
+      "nome": "Giuseppe",
+      "telefono": "333445566",
+      "email": "giuseppe@mail.com"
+    });
+    console.log('Documento inserito =>', insertResult);
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+
+run().catch(console.dir);
+
